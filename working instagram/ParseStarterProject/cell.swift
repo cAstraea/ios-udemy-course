dd83a482b13e87329080caec91a12586a6d63313
Razvan <razvan.comsa@live.com>
2017-01-05 09:17:20 +0200

    added new project

---
+++ working instagram/ParseStarterProject/cell.swift
@@ -0,0 +1,22 @@
+//
+//  cell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Razvan Comsa on 12/30/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+
+
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var username: UILabel!
+    
+    @IBOutlet var message: UILabel!
+
+    
+    
+}
